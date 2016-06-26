7c660da3e5a6f2e93f42d63ded6cecf99269656b
Paul Dardeau <pauldardeau@me.com>
2016-06-26 13:09:24 -0500

    updated rust implementation - almost compiles

--- simulated-disk-io-server.rs
+++ simulated-disk-io-server.rs
@@ -1,48 +1,49 @@
 // rust - simulate occasional problematic (long blocking) requests for disk IO
-// language version: 1.5.0
+// language version: 1.9.0
 // NOTE: does not compile yet
 // to build: rustc simulated-disk-io-server.rs
 
-use std::io::BufferedStream;
-use std::io::net::ip::SocketAddr;
-use std::io::{TcpListener, TcpStream};
-use std::io::{Listener, Writer, Acceptor, Buffer};
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::net::SocketAddr;
+use std::net::{TcpListener, TcpStream};
 use std::process;
-use std::task::spawn;
 use std::thread;
+use std::thread::spawn;
 use std::time::Duration;
 
-const READ_TIMEOUT_SECS: i32 = 4;
+const READ_TIMEOUT_SECS: u32 = 4;
 const STATUS_OK: i32 = 0;
 const STATUS_QUEUE_TIMEOUT: i32 = 1;
 const STATUS_BAD_INPUT: i32 = 2;
 
-fn current_time_millis() -> i64 {
+fn current_time_millis() -> u64 {
     //TODO: time::now()
     return 0;
 }
 
-fn simulated_file_read(disk_read_time_ms: i64) {
+fn simulated_file_read(disk_read_time_ms: u64) {
     thread::sleep(Duration::from_millis(disk_read_time_ms));
 }
 
-fn handle_socket_request(stream: &mut BufferedStream<TcpStream>,
-                         receipt_timestamp: i64) {
+//TODO: review how stream should be accepted
+fn handle_socket_request(stream: TcpStream,
+                         receipt_timestamp: u64) {
     // read request from client
-    let request_text = stream.read_line().unwrap();
-
-    // read from socket succeeded?
-    if errs == nil {
+    let mut request_text = String::new();
+    if stream.read_line(&mut request_text).unwrap() > 0 {
         // did we get anything from client?
         if request_text.trim().len() > 0 {
             // determine how long the request has waited in queue
             let start_processing_timestamp = current_time_millis();
             let server_queue_time_ms =
                 start_processing_timestamp - receipt_timestamp;
-            let server_queue_time_secs = server_queue_time_ms / 1000;
+            let x = server_queue_time_ms / 1000;
+            let server_queue_time_secs = x as u32;
 
             let mut rc = STATUS_OK;
-            let mut disk_read_time_ms = i64(0);
+            let mut disk_read_time_ms: u64 = 0;
             let mut file_path = "";
 
             // has this request already timed out?
@@ -50,19 +51,20 @@ fn handle_socket_request(stream: &mut BufferedStream<TcpStream>,
                 println!("timeout (queue)");
                 rc = STATUS_QUEUE_TIMEOUT;
             } else {
-                let tokens = strings.Split(request_text, ",");
-                if len(tokens) == 3 {
-                    result = tokens[0].parse::<int>();
-                    if errs == nil {
-                        rc_input = result.unwrap();
-                        result =
-                            tokens[1].parse::<int>();
-                        if errs == nil {
-                            req_response_time_ms = result.unwrap();
-                            rc = rc_input;
-                            disk_read_time_ms = req_response_time_ms;
-                            file_path = tokens[2];
-                            simulated_file_read(disk_read_time_ms);
+                let tokens: Vec<&str> = request_text.split(',').collect();
+                if tokens.len() == 3 {
+                    let parse_rc_result = tokens[0].parse();
+                    match parse_rc_result {
+                        Ok(rc_input) => {
+                            let parse_time_result = tokens[1].parse();
+                            match parse_time_result {
+                                Ok(req_response_time_ms) => {
+                                    rc = rc_input;
+                                    disk_read_time_ms = req_response_time_ms;
+                                    file_path = tokens[2];
+                                    simulated_file_read(disk_read_time_ms);
+                                }
+                            }
                         }
                     }
                 }
@@ -80,45 +82,26 @@ fn handle_socket_request(stream: &mut BufferedStream<TcpStream>,
                 file_path);
 
             // return response text to client
-            stream.write_str(response_text);
-            stream.flush();
+            stream.write_all(response_text.as_bytes()).unwrap();
         }
     }
-
-    // close client socket connection
-    //TODO: not needed in Rust?
-    //stream.close();
 }
 
 fn main() {
     let server_port = 7000;
-    let address: SocketAddr  = from_str("0.0.0.0:7000").unwrap();
-    let listener = TcpListener::bind(&address).unwrap();
-    let mut acceptor = listener.listen().unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7000").unwrap();
+    println!("server listening on port {0}", server_port);
 
-    if err != nil {
-        println!("error: unable to create server socket on port {0}",
-                 server_port);
-        println!("{0}", err);
-        process::exit(1);
-    } else {
-        println!("server listening on port {0}", server_port);
-
-        loop {
-            // wait for next socket connection from a client
-            match acceptor.accept() {
-                Err(_) => println!("error listen"),
-                Ok(mut stream) => {
-                    let receipt_timestamp = current_time_millis();
-                    //TODO: run request on separate thread
-                    //spawn(proc() {
-                        let mut stream = BufferedStream::new(stream);
-                        handle_socket_request(stream, receipt_timestamp);
-                    //});
-                }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let receipt_timestamp = current_time_millis();
+                thread::spawn(move|| {
+                    //TODO: review how stream should be passed
+                    handle_socket_request(stream, receipt_timestamp);
+                });
             }
         }
-        process::exit(0);
     }
 }
 
