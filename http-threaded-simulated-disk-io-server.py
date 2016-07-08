66571e438a677d8c2103523ecb62b54d1f19ca7d
Paul Dardeau <pauldardeau@gmail.com>
2016-07-07 22:53:28 -0400

    cleanup of python http threaded

--- http-threaded-simulated-disk-io-server.py
+++ http-threaded-simulated-disk-io-server.py
@@ -6,6 +6,8 @@ import string
 import time
 import socket
 from threading import Thread
+import signal
+import sys
 
 
 READ_TIMEOUT_SECS = 4
@@ -21,11 +23,15 @@ def simulated_file_read(elapsed_time_ms):
 
 
 def handle_socket_request(sock, receipt_timestamp):
-    reader = sock.makefile('r')
-    writer = sock.makefile('w')
-    request_text = reader.readline()
-    reader.close()
-    if request_text:
+    request_text = ''
+    try:
+        buffer_text = sock.recv(1024)
+        if buffer_text is not None and len(buffer_text) > 0:
+            request_text = buffer_text
+    except socket.error:
+        request_text = ''
+    
+    if len(request_text) > 0:
         request_text = request_text.rstrip()
         start_processing_timestamp = time.time()
         queue_time_ms = start_processing_timestamp - receipt_timestamp
@@ -52,29 +58,52 @@ def handle_socket_request(sock, receipt_timestamp):
                     file_path = fields[2]
                     simulated_file_read(disk_read_time_ms)
                 else:
-                    print("len(fields) != 3")
                     rc = HTTP_STATUS_BAD_REQUEST
             else:
-                print("len(line_tokens) <= 0")
                 rc = HTTP_STATUS_BAD_REQUEST
 
         # total request time is sum of time spent in queue and the
         # simulated disk read time
         tot_request_time_ms = queue_time_ms + disk_read_time_ms
+    else:
+        rc = HTTP_STATUS_BAD_REQUEST
+        tot_request_time_ms = 0
+        file_path = ''
 
-        # construct response and send back to client
-        resp_text = 'HTTP/1.1 %s\n' % rc
-        resp_text += "%s\n" % SERVER_NAME
-        resp_text += "Connection: close\n\n"
-        resp_text += "%d,%s" % \
-            (tot_request_time_ms, file_path)
-        writer.write(resp_text)
-        writer.flush()
-    writer.close()
-    sock.close()
+    # construct response and send back to client
+    response_body = "%d,%s" % \
+        (tot_request_time_ms, file_path)
+
+    response_text = 'HTTP/1.1 %s\n' % rc
+    response_text += "%s\n" % SERVER_NAME
+    response_text += "Content-Length: %d\n" % len(response_body)
+    response_text += "Connection: close\n"
+    response_text += "\n"
+    response_text += response_body
+
+    try:
+        sock.send(response_text)
+    except socket.error:
+        pass
+
+    try:
+        sock.close()
+    except socket.error:
+        pass
+
+
+def do_nothing_handler(sig, dummy):
+    pass
+
+
+def exit_handler(sig, dummy):
+    global server_socket
+    server_socket.shutdown(socket.SHUT_RDWR)
+    sys.exit(0)
 
 
 def main(server_port):
+    global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('', server_port))
@@ -83,14 +112,19 @@ def main(server_port):
 
     try:
         while True:
-            sock, addr = server_socket.accept()
+            try:
+                sock, addr = server_socket.accept()
+            except socket.error:
+                continue
             receipt_timestamp = time.time()
             Thread(target=lambda: handle_socket_request(sock, receipt_timestamp)).start()
     except KeyboardInterrupt:
-        pass  # exit
+        sys.exit(0)
 
 
 if __name__=='__main__':
+    signal.signal(signal.SIGINT, exit_handler)
+    signal.signal(signal.SIGPIPE, do_nothing_handler)
     server_port = 7000
     main(server_port)
 
