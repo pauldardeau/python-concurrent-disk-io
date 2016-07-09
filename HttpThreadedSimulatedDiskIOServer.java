ecd0834af392a7e2931a0397d34386a1b220fac8
Paul Dardeau <pauldardeau@gmail.com>
2016-07-09 11:01:57 -0500

    complete java http

--- HttpThreadedSimulatedDiskIOServer.java
+++ HttpThreadedSimulatedDiskIOServer.java
@@ -1,4 +1,8 @@
 // Java - simulate occasional problematic (long blocking) requests for disk IO
+// install tools:
+//    sudo add-apt-repository ppa:openjdk-r/ppa
+//    sudo apt-get update
+//    sudo apt-get install openjdk-8-jdk
 // to build: javac HttpThreadedSimulatedDiskIOServer.java
 // to run: java HttpThreadedSimulatedDiskIOServer
 
@@ -10,7 +14,9 @@ import java.util.*;
 
 public class HttpThreadedSimulatedDiskIOServer {
 
-    public static final int READ_TIMEOUT_SECS = 4;
+    public static final int SERVER_PORT = 7000;
+    public static final int QUEUE_TIMEOUT_SECS = 4;
+    public static final int LISTEN_BACKLOG = 500;
 
     public static final String SERVER_NAME = "HttpThreadedSimulatedDiskIOServer.java";
     public static final String HTTP_STATUS_OK = "200 OK";
@@ -38,6 +44,9 @@ public class HttpThreadedSimulatedDiskIOServer {
 
             // read request from client
             String request_text = reader.readLine();
+            String rc = HTTP_STATUS_OK;
+            String file_path = "";
+            long tot_request_time_ms = 0;
 
             // did we get anything from client?
             if ((request_text != null) && (request_text.length() > 0)) {
@@ -47,25 +56,30 @@ public class HttpThreadedSimulatedDiskIOServer {
                 receipt_timestamp;
                 final int queue_time_secs = (int) (queue_time_ms / 1000);
 
-                String rc = HTTP_STATUS_OK;
                 long disk_read_time_ms = 0;
-                String file_path = "";
 
                 // has this request already timed out?
-                if (queue_time_secs >= READ_TIMEOUT_SECS) {
-                System.out.println("timeout (queue)");
+                if (queue_time_secs >= QUEUE_TIMEOUT_SECS) {
+                    System.out.println("timeout (queue)");
                     rc = HTTP_STATUS_TIMEOUT;
                 } else {
                     StringTokenizer stRequestLine = new StringTokenizer(request_text, " ");
                     if (stRequestLine.countTokens() > 1) { 
                         // skip over the first one (http verb)
                         stRequestLine.nextToken();
-                        StringTokenizer st = new StringTokenizer(stRequestLine.nextToken(),",");
-                        if (st.countTokens() == 3) {
-                            int rc_input = Integer.parseInt(st.nextToken());
-                            disk_read_time_ms = Long.parseLong(st.nextToken());
-                            file_path = st.nextToken();
-                            simulated_file_read(disk_read_time_ms);
+                        StringTokenizer stArgs =
+                            new StringTokenizer(stRequestLine.nextToken(),",");
+                        if (stArgs.countTokens() == 3) {
+                            try {
+                                int rc_input =
+                                    Integer.parseInt(stArgs.nextToken());
+                                disk_read_time_ms =
+                                    Long.parseLong(stArgs.nextToken());
+                                file_path = stArgs.nextToken();
+                                simulated_file_read(disk_read_time_ms);
+                            } catch (Exception e) {
+                                rc = HTTP_STATUS_BAD_REQUEST;
+                            }
                         } else {
                             rc = HTTP_STATUS_BAD_REQUEST;
                         }
@@ -76,20 +90,21 @@ public class HttpThreadedSimulatedDiskIOServer {
 
                 // total request time is sum of time spent in queue and the
                 // simulated disk read time
-                final long tot_request_time_ms =
+                tot_request_time_ms =
                     queue_time_ms + disk_read_time_ms;
-
-                // return response to client
-                String response_body = "" + tot_request_time_ms + "," + file_path;
-
-                String response_headers = "HTTP/1.1 " + rc + "\n" +
-                                          "Server: " + SERVER_NAME + "\n" +
-                                          "Content-Length: " + response_body.length() + "\n" +
-                                          "\n";
-                writer.write(response_headers);
-                writer.write(response_body);
-                writer.flush();
             }
+
+            // return response to client
+            String response_body = "" + tot_request_time_ms + "," + file_path;
+
+            String response_headers = "HTTP/1.1 " + rc + "\n" +
+                                      "Server: " + SERVER_NAME + "\n" +
+                                      "Content-Length: " + response_body.length() + "\n" +
+                                      "Connection: close\n" +
+                                      "\n";
+            writer.write(response_headers);
+            writer.write(response_body);
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -115,16 +130,21 @@ public class HttpThreadedSimulatedDiskIOServer {
     }
 
     public static void main(String[] args) {
-        int server_port = 7000;
         try {
-            ServerSocket server_socket = new ServerSocket(server_port);
-            System.out.println("server listening on port " + server_port);
+            ServerSocket server_socket =
+                new ServerSocket(SERVER_PORT, LISTEN_BACKLOG);
+            server_socket.setReuseAddress(true);
+            System.out.println("server (" +
+                               SERVER_NAME +
+                               ") listening on port " +
+                               SERVER_PORT);
 
             while (true) {
                 try {
                     final Socket sock = server_socket.accept();
                     if (sock != null) {
-                        final long receipt_timestamp = System.currentTimeMillis();
+                        final long receipt_timestamp =
+                            System.currentTimeMillis();
                         new Thread(new Runnable() {
                             @Override
                             public void run() {
@@ -136,7 +156,7 @@ public class HttpThreadedSimulatedDiskIOServer {
                 }
             }
         } catch (IOException e) {
-            System.out.println("error: unable to create server socket on port " + server_port);
+            System.out.println("error: unable to create server socket on port " + SERVER_PORT);
         }
     }
 
