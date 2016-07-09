476d79cc950ac1d17105c7474a6917e49d696069
Paul Dardeau <pauldardeau@gmail.com>
2016-07-09 10:34:22 -0500

    complete c# http

--- HttpThreadedSimulatedDiskIOServer.cs
+++ HttpThreadedSimulatedDiskIOServer.cs
@@ -1,9 +1,11 @@
 // C# - simulate occasional problematic (long blocking) requests for disk IO
+// install tools: sudo apt-get install mono-complete
 // to build: mcs HttpThreadedSimulatedDiskIOServer.cs
 // to run: mono HttpThreadedSimulatedDiskIOServer.exe
 
 using System;
 using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
@@ -14,11 +16,14 @@ namespace diskioserver {
 
 public class HttpThreadedSimulatedDiskIOServer {
 
-public static int READ_TIMEOUT_SECS = 4;
+public static int SERVER_PORT = 7000;
+public static int QUEUE_TIMEOUT_SECS = 4;
+public static int LISTEN_BACKLOG = 500;
 
-public static int HTTP_STATUS_OK = 200;
-public static int HTTP_STATUS_TIMEOUT = 418;
-public static int HTTP_STATUS_BAD_REQUEST = 400;
+public static string SERVER_NAME = "HttpThreadedSimulatedDiskIOServer.cs";
+public static string HTTP_STATUS_OK = "200 OK";
+public static string HTTP_STATUS_TIMEOUT = "408 TIMEOUT";
+public static string HTTP_STATUS_BAD_REQUEST = "400 BAD REQUEST";
 
 
 public static long current_time_millis() {
@@ -29,17 +34,20 @@ public static void simulated_file_read(long disk_read_time_ms) {
     Thread.Sleep((int)disk_read_time_ms);
 }
 
-public static void handle_socket_request(TcpClient client_socket,
+public static void handle_socket_request(Socket client_socket,
                                          long receipt_timestamp) {
-    StreamReader reader = null;
-    StreamWriter writer = null;
 
     try {
-        reader = new StreamReader(client_socket.GetStream(), Encoding.UTF8);
-        writer = new StreamWriter(client_socket.GetStream());
-
+        byte[] buffer = new Byte[1024];
         // read request from client
-        string request_text = reader.ReadLine();
+        int bytes_read = client_socket.Receive(buffer);
+        // read request from client
+        string request_text =
+            Encoding.ASCII.GetString(buffer, 0, bytes_read);
+
+        string rc = HTTP_STATUS_OK;
+        long tot_request_time_ms = 0;
+        string file_path = "";
 
         // did we get anything from client?
         if ((request_text != null) && (request_text.Length > 0)) {
@@ -49,66 +57,84 @@ public static void handle_socket_request(TcpClient client_socket,
                 receipt_timestamp;
             int queue_time_secs = (int) (queue_time_ms / 1000);
 
-            int rc = HTTP_STATUS_OK;
             long disk_read_time_ms = 0;
-            string file_path = "";
 
             // has this request already timed out?
-            if (queue_time_secs >= READ_TIMEOUT_SECS) {
+            if (queue_time_secs >= QUEUE_TIMEOUT_SECS) {
                 Console.WriteLine("timeout (queue)");
                 rc = HTTP_STATUS_TIMEOUT;
             } else {
-                string[] tokens = request_text.Split(',');
-                if (tokens.Length == 3) {
-                    rc = Convert.ToInt32(tokens[0]);
-                    disk_read_time_ms = Convert.ToInt64(tokens[1]);
-                    file_path = tokens[2];
-                    simulated_file_read(disk_read_time_ms);
+                string[] input_lines = request_text.Split('\n');
+                if (input_lines.Length > 0) {
+                    string request_line = input_lines[0];
+                    string[] line_tokens = request_line.Split(' ');
+                    if (line_tokens.Length > 1) {
+                        string args = line_tokens[1];
+                        string[] fields = args.Split(',');
+                        if (fields.Length == 3) {
+                            int rc_input = Convert.ToInt32(fields[0]);
+                            disk_read_time_ms = Convert.ToInt64(fields[1]);
+                            file_path = fields[2];
+                            simulated_file_read(disk_read_time_ms);
+                        }
+                    }
                 }
             }
 
             // total request time is sum of time spent in queue and the
             // simulated disk read time
-            long tot_request_time_ms =
+            tot_request_time_ms =
                 queue_time_ms + disk_read_time_ms;
-
-            // return response text to client
-            writer.Write("" + rc + "," +
-                         tot_request_time_ms + "," +
-                         file_path + "\n");
-            writer.Flush();
         }
-    } catch (Exception e) {
-        //e.printStackTrace();
+
+        // return response to client
+        string response_body = "" + tot_request_time_ms + "," + file_path;
+
+        string response_headers = "HTTP/1.1 " + rc + "\n";
+        response_headers +=  "Server: " + SERVER_NAME + "\n";
+        response_headers += "Content-Length: " + response_body.Length + "\n";
+        response_headers += "Connection: close\n";
+        response_headers += "\n";
+
+        byte[] raw_response_headers =
+            Encoding.ASCII.GetBytes(response_headers);
+        client_socket.Send(raw_response_headers);
+
+        byte[] raw_response_body = Encoding.ASCII.GetBytes(response_body);
+        client_socket.Send(raw_response_body);
+        client_socket.Shutdown(SocketShutdown.Both);
+    } catch (Exception) {
     } finally {
-        if (reader != null) {
-            reader.Close();
-        }
-
-        if (writer != null) {
-            writer.Close();
-        }
-
         // close client socket connection
         try {
             client_socket.Close();
-        } catch (Exception ignored) {
+        } catch (Exception) {
         }
     }
 }
 
 public static void Main(string[] args) {
-    int server_port = 7000;
     try {
-        TcpListener server_socket = new TcpListener(server_port);
-        //TODO: set listen backlog?
-        server_socket.Start();
-        Console.WriteLine("server listening on port " + server_port);
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Loopback,
+                                                  SERVER_PORT);
+        Socket server_socket =
+            new Socket(IPAddress.Loopback.AddressFamily,
+                       SocketType.Stream,
+                       ProtocolType.Tcp);
+        server_socket.SetSocketOption(SocketOptionLevel.Socket,
+                                      SocketOptionName.ReuseAddress,
+                                      true);
+        server_socket.Bind(localEndPoint);
+        server_socket.Listen(LISTEN_BACKLOG);
+
+        Console.WriteLine("server (" +
+                          SERVER_NAME +
+                          ") listening on port " +
+                          SERVER_PORT);
 
         while (true) {
             try {
-                TcpClient client_socket = default(TcpClient);
-                client_socket = server_socket.AcceptTcpClient();
+                Socket client_socket = server_socket.Accept();
                 if (client_socket != null) {
                     long receipt_timestamp = current_time_millis();
                     Thread thread =
@@ -116,11 +142,12 @@ public static void Main(string[] args) {
                                                                receipt_timestamp));
                     thread.Start();
                 }
-            } catch (Exception ignored) {
+            } catch (Exception) {
             }
         }
-    } catch (Exception e) {
-        Console.WriteLine("error: unable to create server socket on port " + server_port);
+    } catch (Exception) {
+        Console.WriteLine("error: unable to create server socket on port " +
+                          SERVER_PORT);
     }
 }
 
